38ebf511747942378513be116e054c2fe2effca8
Ian Scrivener <ian.scrivener@cabcharge.com.au>
2016-08-15 17:45:41 +1000

    bump node version

--- bin/server.js
+++ bin/server.js
@@ -11,18 +11,18 @@ global.__DISABLE_SSR__ = false;  // <----- DISABLES SERVER SIDE RENDERING FOR ER
 global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
 
 if (__DEVELOPMENT__) {
-  if (!require('piping')({
-      hook: true,
-      ignore: /(\/\.|~$|\.json|\.scss$)/i
-    })) {
-    return;
-  }
+    if (!require('piping')({
+            hook: true,
+            ignore: /(\/\.|~$|\.json|\.scss$)/i
+        })) {
+        return;
+    }
 }
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
 var WebpackIsomorphicTools = require('webpack-isomorphic-tools');
 global.webpackIsomorphicTools = new WebpackIsomorphicTools(require('../webpack/webpack-isomorphic-tools'))
-  .development(__DEVELOPMENT__)
-  .server(rootDir, function() {
-    require('../src/server');
-  });
+    .development(__DEVELOPMENT__)
+    .server(rootDir, function () {
+        require('../src/server');
+    });
